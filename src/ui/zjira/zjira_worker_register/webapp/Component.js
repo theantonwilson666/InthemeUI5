0377e37fad7c403ad34f38dcd36212f31592a690
theantonwilson666 <theantonwilson@yandex.ru>
2021-03-30 00:30:45 +0300

    [29.03.2021] WorkerRegister create

---
+++ src/ui/zjira/zjira_worker_register/webapp/Component.js
@@ -0,0 +1,33 @@
+sap.ui.loader.config({paths: {"jira/lib": "/sap/bc/ui5_ui5/sap/zjiralib"}});
+
+sap.ui.define([
+    "jira/lib/Component",
+    "intheme/zjira_worker_register/model/models",
+    "sap/ui/Device"
+], function (UIComponent, models, Device) {
+    "use strict";
+
+    return UIComponent.extend("intheme.zjira_worker_register.Component", {
+
+        metadata: {
+            manifest: "json",
+        },
+
+        /**
+         * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+         * @public
+         * @override
+         */
+        
+        init: function () {
+            // call the base component's init function
+            UIComponent.prototype.init.apply(this, arguments);
+            
+            // enable routing
+            this.getRouter().initialize();
+            
+            // set the device model
+            this.setModel(models.createDeviceModel(),"device");
+        }
+    });
+});
---
+++ src/ui/zjira/zjira_worker_register/webapp/controller/Main.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define(
+    [
+      "jira/lib/BaseController",
+    ],
+    function (BaseController) {
+      "use strict";
+  
+      return BaseController.extend("intheme.zjira_worker_register.controller.Main", {
+        onInit: function () {
+        }
+  
+      });
+    }
+  );
+  
