c56eb7f0fb4e23d7ad063e52edd05c9c8d5791ba
theantonwilson666 <theantonwilson@yandex.ru>
2022-02-08 14:19:02 +0300

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,7 +62,7 @@ module.exports = function (grunt) {
 
       {
         package: "ZWORKER_SCHEDULE",
-        transportno: "TMDK921753",
+        transportno: "TMDK922192",
         project: "zjira",
         apps: {
           zworker_schedule: {
