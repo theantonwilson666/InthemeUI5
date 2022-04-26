8e7d17e166ee00ef5ef654561f1963809bbd650f
theantonwilson666 <theantonwilson@yandex.ru>
2022-04-26 12:15:02 +0300

    update

--- src/ui/intime/zissues_workspace/webapp/controller/Main.controller.js
+++ src/ui/intime/zissues_workspace/webapp/controller/Main.controller.js
@@ -5,12 +5,25 @@ sap.ui.define([
     'sap/m/MessageBox',
     'sap/m/Button'
 ],
-function(BaseController, Fragment, MessageBox, Button) {
-    "use strict";
+    function (BaseController, Fragment, MessageBox, Button) {
+        "use strict";
 
-    return BaseController.extend("intime.zissues_workspace.controller.Main", {
-        onInit: function() {
-        }
+        return BaseController.extend("intime.zissues_workspace.controller.Main", {
+            onInit: function () {
+            },
 
+            onChooseProjectTitlePress: function () {
+                this.loadDialog
+                    .call(this, {
+                        sDialogName: "_chooseProjectDialog",
+                        sViewName: "intime.zissues_workspace.view.fragments.chooseProjectDialog"
+                    })
+                    .then(
+                        function (oDialog) {
+                            oDialog.open();
+                        }.bind(this)
+                    );
+            }
+
+        });
     });
-});
