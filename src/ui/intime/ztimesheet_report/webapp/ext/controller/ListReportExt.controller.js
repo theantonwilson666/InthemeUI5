5cd1ca68d89b6f4942d34e6fe59a77684498aefd
theantonwilson666 <theantonwilson@yandex.ru>
2022-05-25 16:50:09 +0300

    update

---
+++ src/ui/intime/ztimesheet_report/webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,11 @@
+sap.ui.controller("intime.ts_report.ext.controller.ListReportExt", {
+    onInit: function() {
+
+        // this.getView().attachAfterRendering((oEvent) => {
+        //     oEvent.getSource().getContent()[0].getHeader()._getCollapseButton().firePress()
+        // })
+        
+        // this.uiExtensions();
+    }
+});
+
