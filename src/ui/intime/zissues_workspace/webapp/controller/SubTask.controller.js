5534274b58648420862b7230140b834e469c573c
theantonwilson666 <theantonwilson@yandex.ru>
2022-06-03 01:12:32 +0300

    update

--- src/ui/intime/zissues_workspace/webapp/controller/SubTask.controller.js
+++ src/ui/intime/zissues_workspace/webapp/controller/SubTask.controller.js
@@ -1,30 +1,30 @@
 sap.ui.define([
-    // "intime.zissues_workspace.controller.App",
-    "intime/zissues_workspace/controller/Detail.controller",
-    'sap/ui/core/Fragment',
-    'sap/m/MessageBox',
-    'sap/m/Button',
-    "sap/ui/model/json/JSONModel"
-],
-    function (DetailController, Fragment, MessageBox, Button, JSONModel) {
+        // "intime.zissues_workspace.controller.App",
+        "intime/zissues_workspace/controller/Detail.controller",
+        'sap/ui/core/Fragment',
+        'sap/m/MessageBox',
+        'sap/m/Button',
+        "sap/ui/model/json/JSONModel"
+    ],
+    function(DetailController, Fragment, MessageBox, Button, JSONModel) {
         "use strict";
 
         return DetailController.extend("intime.zissues_workspace.controller.SubTask", {
-            onInit: function () {
+            onInit: function() {
                 this.getRouter()
                     .getRoute("subtask")
                     .attachPatternMatched(this._onRouteMatched, this);
 
             },
 
-            _onRouteMatched: function (oEvent) {
+            _onRouteMatched: function(oEvent) {
                 this._routeSubTaskParam = {
                     taskId: atob(oEvent.getParameter("arguments").taskId),
                     subTaskId: atob(oEvent.getParameter("arguments").subTaskId),
                     param: oEvent.getParameter("arguments")["?query"]
                 };
 
-                this.getView().getModel().metadataLoaded().then(function () {
+                this.getView().getModel().metadataLoaded().then(function() {
 
                     var oTaskContext = this.getStateProperty("/taskContext");
                     if (oTaskContext) {
@@ -49,14 +49,14 @@ sap.ui.define([
                                 TaskID: this._routeSubTaskParam.taskId
                             },
 
-                            success: function (oData) {
+                            success: function(oData) {
                                 this.getView().setBusy(false);
                                 this.isExistError();
 
                                 var oNewSubTaskContext = this.getView().getModel().createEntry(`/ZSNN_INTIME_SUBTASK`, {
                                     properties: oData,
                                     groupId: "changes",
-                                    success: function (oData) {
+                                    success: function(oData) {
                                         this.navTo("subtask", {
                                             taskId: btoa(oData.TaskId),
                                             subTaskId: btoa(oData.SubtaskId)
@@ -70,7 +70,7 @@ sap.ui.define([
 
                             }.bind(this),
 
-                            error: function (oError) {
+                            error: function(oError) {
                                 this.getView().setBusy(false);
                                 this.showError(oError);
                             }.bind(this)
@@ -92,7 +92,7 @@ sap.ui.define([
                             },
                             events: {
 
-                                dataReceived: function (oData) {
+                                dataReceived: function(oData) {
                                     var oTaskModel = new JSONModel(oData.getParameter("data").to_Task);
                                     this.getView().setModel(oTaskModel, "taskData");
                                     this.setFaviconIconByPartner(oData.getParameter("data").to_Task.PartnerID);
@@ -100,6 +100,8 @@ sap.ui.define([
                             }
                         });
                     }
+
+                    this.byId("_SubTaskTimeSheet-SmartTable").bindProperty("editable", "state>/subTaskEditMode");
 
                 }.bind(this));
 
@@ -109,14 +111,38 @@ sap.ui.define([
             },
 
 
-            getTaskData: function () {
+            onDeleteTimesheet: function(oEvent) {
+
+                this._deletedTimeSheet = oEvent.getParameter('listItem').getBindingContext();
+
+                sap.m.MessageBox.warning("Удалить списание?", {
+                    actions: [sap.m.MessageBox.Action.OK, sap.m.MessageBox.Action.CANCEL],
+                    emphasizedAction: sap.m.MessageBox.Action.OK,
+                    onClose: function(sAction) {
+                        if (sAction === 'OK') {
+                            this.getModel().remove(this._deletedTimeSheet.getPath(), {
+                                success: function(oData) {
+                                    this.isExistError();
+                                    this.refreshPage();
+                                }.bind(this),
+
+                                error: function(oError) {
+                                    this.showError(oError);
+                                }.bind(this)
+                            });;
+                        }
+                    }.bind(this)
+                });;
+            },
+
+            getTaskData: function() {
 
                 if (this.getView().getBindingContext().bCreated) {
                     this.goToMainPage(true);
                     return;
                 }
 
-                return new Promise(function (resolve, reject) {
+                return new Promise(function(resolve, reject) {
                     this.getModel().read(this.getModel().getBindingContext().getPath() + "/to_Task", {
                         success: resolve,
                         error: reject
@@ -125,11 +151,11 @@ sap.ui.define([
             },
 
 
-            onSubTaskEditButtonPress: function () {
+            onSubTaskEditButtonPress: function() {
                 this.setStateProperty("/subTaskEditMode", !this.getStateProperty("/subTaskEditMode"));
             },
 
-            onSubTaskDeleteButtonPress: function (oEvent) {
+            onSubTaskDeleteButtonPress: function(oEvent) {
 
                 this._delSubTask = oEvent.getSource().getBindingContext();
 
@@ -137,16 +163,16 @@ sap.ui.define([
 
                 MessageBox.warning(`Удалить подзадачу "${this._delSubTask.getObject().Name}"?`, {
                     actions: [MessageBox.Action.OK, MessageBox.Action.CANCEL],
-                    onClose: function (sAction) {
+                    onClose: function(sAction) {
                         this.getView().setBusy(false);
                         if (sAction === 'OK') {
 
                             this.getModel().remove(this._delSubTask.getPath(), {
-                                success: function (oData) {
+                                success: function(oData) {
                                     this.isExistError();
                                 }.bind(this),
 
-                                error: function (oError) {
+                                error: function(oError) {
                                     this.showError(oError);
                                 }.bind(this)
                             });
@@ -157,16 +183,16 @@ sap.ui.define([
 
             },
 
-            getPage: function () {
+            getPage: function() {
                 return this.byId("subTaskPage");
             },
 
-            onSaveSubTaskButtonPress: function () {
+            onSaveSubTaskButtonPress: function() {
                 this.getPage().setBusy(true);
 
                 this.submitChanges({
                     groupId: "changes",
-                    success: function () {
+                    success: function() {
                         this.getPage().setBusy(false);
 
                         if (!this.isExistError()) {
@@ -174,7 +200,7 @@ sap.ui.define([
                             this.refreshPage();
                         }
                     }.bind(this),
-                    error: function (oError) {
+                    error: function(oError) {
                         this.getPage().setBusy(false);
                         this.showError(oError);
                         this.refreshPage();
@@ -183,18 +209,18 @@ sap.ui.define([
                 });
             },
 
-            onRejectSubTaskButtonPress: function () {
+            onRejectSubTaskButtonPress: function() {
                 this.getView().getModel().resetChanges();
                 this.setStateProperty("/subTaskEditMode", false);
 
             },
 
-            afterCloseDialog: function (oEvent) {
+            afterCloseDialog: function(oEvent) {
                 debugger;
             },
 
 
-            onAddSubTaskExecutor: function (oEvent) {
+            onAddSubTaskExecutor: function(oEvent) {
                 debugger;
                 this.loadDialog
                     .call(this, {
@@ -202,7 +228,7 @@ sap.ui.define([
                         sViewName: "intime.zissues_workspace.view.SubTaskSection.NewExecutorDialog"
                     })
                     .then(
-                        function (oDialog) {
+                        function(oDialog) {
 
                             var oExecutorContext = this.getModel().createEntry(this.getPage().getBindingContext().getPath() + "/to_Executors", {
                                 groupId: "changes",
@@ -220,19 +246,19 @@ sap.ui.define([
             },
 
 
-            onOkNewExecutorDialog: function (oEvent) {
+            onOkNewExecutorDialog: function(oEvent) {
                 this.onSaveSubTaskButtonPress();
                 oEvent.getSource().getParent().close();
                 this.onRemoveRowSelection();
 
             },
 
-            onCancelNewExecutorDialog: function (oEvent) {
+            onCancelNewExecutorDialog: function(oEvent) {
                 oEvent.getSource().getParent().close();
                 this.onRemoveRowSelection();
             },
 
-            onDeleteSelection: function (oEvent) {
+            onDeleteSelection: function(oEvent) {
                 debugger
 
                 var oItem = oEvent.getSource().getParent();
@@ -241,7 +267,7 @@ sap.ui.define([
                 sap.m.MessageBox.warning(`Вы уверены, что хотите удалить исполнителя ${oEntry.ExecutorName}`, {
                     actions: [MessageBox.Action.OK, MessageBox.Action.CANCEL],
                     emphasizedAction: MessageBox.Action.OK,
-                    onClose: function (sAction) {
+                    onClose: function(sAction) {
                         switch (sAction) {
                             case MessageBox.Action.OK:
                                 this.getModel().remove(oItem.getBindingContext().sPath);
@@ -257,7 +283,7 @@ sap.ui.define([
             },
 
 
-            onTimeSheetCreateButtonPress: function (oEvent) {
+            onTimeSheetCreateButtonPress: function(oEvent) {
 
                 debugger;
 
@@ -269,14 +295,14 @@ sap.ui.define([
 
                     })
                     .then(
-                        function (oDialog) {
+                        function(oDialog) {
 
                             oDialog.open();
 
-                            oDialog.timeSheetSaveResult.then(function (oSuccess) {
+                            oDialog.timeSheetSaveResult.then(function(oSuccess) {
                                 this.isExistError();
                                 this.refreshPage();
-                            }.bind(this), function (oError) {
+                            }.bind(this), function(oError) {
                                 this.showError(oError);;
                             }.bind(this));
 
@@ -285,7 +311,7 @@ sap.ui.define([
                     );;
             },
 
-            onRemoveRowSelection: function () {
+            onRemoveRowSelection: function() {
                 var oFirstSelectedItem = this.byId("_SubTaskExecutors-SmartTable").getTable().getSelectedItems()[0];
                 this.byId("_SubTaskExecutors-SmartTable").getTable().setSelectedItem(oFirstSelectedItem, false);
             },
@@ -295,7 +321,7 @@ sap.ui.define([
             },
 
             onMoveToAnotherSubTaskButtonPress: function() {
-                
+
             }
         });
     });
