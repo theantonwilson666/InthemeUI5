186c0d0bfc2502d90e797b363299e34a76da7890
theantonwilson666 <theantonwilson@yandex.ru>
2022-06-30 20:10:15 +0300

    update

---
+++ src/ui/notifications/znotif_registry/webapp/Component.js
@@ -0,0 +1,8 @@
+sap.ui.loader.config({ paths: { "jira/lib": "/sap/bc/ui5_ui5/sap/zjiralib" } });
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function (AppComponent) {
+    return AppComponent.extend("znotif_reg.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
---
+++ src/ui/notifications/znotif_registry/webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,15 @@
+$.sap.require("sap/ui/core/Fragment");
+var Fragment = sap.ui.require("sap/ui/core/Fragment");
+
+
+
+sap.ui.controller("znotif_reg.ext.controller.ListReportExt", {
+
+  // _TextEditorModel : null,
+
+  onInit: function () {
+    // this.extensionAPI.attachPageDataLoaded(this.pageDataLoaded.bind(this))
+  }
+
+});
+
