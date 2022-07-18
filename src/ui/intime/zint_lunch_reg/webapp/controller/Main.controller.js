fa771c914bec3ef4891154cf34f2786659bbaa80
theantonwilson666 <theantonwilson@yandex.ru>
2022-07-18 12:48:18 +0300

    update

--- src/ui/intime/zint_lunch_reg/webapp/controller/Main.controller.js
+++ src/ui/intime/zint_lunch_reg/webapp/controller/Main.controller.js
@@ -1,12 +1,12 @@
 sap.ui.define([
-    "jira/lib/BaseController"
+        "jira/lib/BaseController"
 
-],
-    function (BaseController) {
+    ],
+    function(BaseController) {
         "use strict";
 
         return BaseController.extend("intime.zint_lunch_reg.controller.Main", {
-            onInit: function () {
+            onInit: function() {
                 this.getRouter()
                     .getRoute("mainpage")
                     .attachPatternMatched(this._onRouteMatched, this);
@@ -14,44 +14,44 @@ sap.ui.define([
             },
 
 
-            _onRouteMatched: function () {
+            _onRouteMatched: function() {
                 this._setFilters();
             },
 
 
-            _setFilters: function () {
+            _setFilters: function() {
                 //Первые блюда
                 this.byId('_Soup-GridList').getBinding('items').filter([
-                    new sap.ui.model.Filter({
-                        path: "DishType",
-                        operator: "EQ",
-                        value1: "FC"
-                    })
-                ]),
-                 //Горячее
-                this.byId('_Hot-GridList').getBinding('items').filter([
-                            new sap.ui.model.Filter({
-                                path: "DishType",
-                                operator: "EQ",
-                                value1: "HD"
-                            })
-                        ]),
-                //Гарнир
-                this.byId('_Garnish-GridList').getBinding('items').filter([
-                            new sap.ui.model.Filter({
-                                path: "DishType",
-                                operator: "EQ",
-                                value1: "G"
-                           })
-                        ]),
-                //Закуски
-                this.byId('_Snacks-GridList').getBinding('items').filter([
-                            new sap.ui.model.Filter({
-                                path: "DishType",
-                                operator: "EQ",
-                                value1: "ST"
-                                   })
-                                ]);
+                        new sap.ui.model.Filter({
+                            path: "DISH_TYPE",
+                            operator: "EQ",
+                            value1: "FC"
+                        })
+                    ]),
+                    //Горячее
+                    this.byId('_Hot-GridList').getBinding('items').filter([
+                        new sap.ui.model.Filter({
+                            path: "DISH_TYPE",
+                            operator: "EQ",
+                            value1: "HD"
+                        })
+                    ]),
+                    //Гарнир
+                    this.byId('_Garnish-GridList').getBinding('items').filter([
+                        new sap.ui.model.Filter({
+                            path: "DISH_TYPE",
+                            operator: "EQ",
+                            value1: "G"
+                        })
+                    ]),
+                    //Закуски
+                    this.byId('_Snacks-GridList').getBinding('items').filter([
+                        new sap.ui.model.Filter({
+                            path: "DISH_TYPE",
+                            operator: "EQ",
+                            value1: "ST"
+                        })
+                    ]);
 
                 debugger;
             },
