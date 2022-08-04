bfbffa78d8ec16db62bcdfea8f4b3f5962a1c95e
Вильсон Антон Юрьевич <vilson.anton@tls-russia.ru>
2022-08-04 17:43:13 +0300

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -262,8 +262,8 @@ module.exports = function(grunt) {
                 options: {
                     appresources: "src",
 
-                    resources: "C:/ui5/resources",
-                    testresources: "C:/ui5/test-resources",
+                    resources: "C:/work/ui5_resources/sap-ui5-1.71.44/resources",
+                    testresources: "C:/work/ui5_resources/sap-ui5-1.71.44/test-resources",
 
                     proxypath: "proxy",
                 },
